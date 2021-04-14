09e6bbf3682122449b574ba8915b3f2c5c3a842b
YOGESHWARAN R <yogeshin247@gmail.com>
2021-04-14 10:21:14 +0530

    Code optimized

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,21 @@
-from .initial import *
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_basicauth import BasicAuth
+
+from .config import Configuration
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+database = SQLAlchemy(app)
+migrate = Migrate(app, database)
+
+admin = Admin(app, name="ADMIN")
+
+auth = BasicAuth(app)
+
 from .models.content import *
 from .models.books import *
 from .models.contact import *
