60bcd676ebb914e272b87a1f96d0ae1331bcba79
YOGESHWARAN R <yogeshin247@gmail.com>
2021-04-23 15:06:42 +0530

    updated config to deployment

--- app/config.py
+++ app/config.py
@@ -6,13 +6,13 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 class Configuration:
     """ Configurations for Flask app """
 
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY = "admin"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir, "data.db")
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SECRET_KEY = "admin"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir, "data.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ADMIN_SWATCH = "cerulean"
-    # BASIC_AUTH_USERNAME = os.environ.get('ADMIN_USER')
-    # BASIC_AUTH_PASSWORD = os.environ.get('ADMIN_PASS')
-    BASIC_AUTH_USERNAME = "admin"
-    BASIC_AUTH_PASSWORD = "admin"
+    BASIC_AUTH_USERNAME = os.environ.get('ADMIN_USER')
+    BASIC_AUTH_PASSWORD = os.environ.get('ADMIN_PASS')
+    # BASIC_AUTH_USERNAME = "admin"
+    # BASIC_AUTH_PASSWORD = "admin"
