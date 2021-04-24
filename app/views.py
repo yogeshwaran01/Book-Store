7b4caa37b4132d8e969798eface3fd56c9721395
YOGESHWARAN R <yogeshin247@gmail.com>
2021-04-24 15:59:15 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -136,15 +136,6 @@ admin.add_view(Authenticate(Contact, db.session))
 admin.add_view(Authenticate(Posts, db.session))
 admin.add_view(Authenticate(Note, db.session))
 
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
 
 import os
 from flask import url_for
